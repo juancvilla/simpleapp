0c3b3746f14ccfff6025f479f2be595a2aa7fa5e
juancvilla <juancvilla@gmail.com>
2023-04-01 22:19:04 +0000

    changes 2

--- index.php
+++ index.php
@@ -9,8 +9,5 @@
 		<p><b>An juancvilla Example of PHP in Action</b></p>
 		<?php echo "The Current Date and Time is: <br />"; 
 		echo date("g:i A l, F j Y.");?> </p>
-	<h2>PHP Information</h2> 
-		<p> <?php phpinfo(); ?> </p> 
-        <h2>TIME FOR A ...</h2>
 	</body> 
 </html>
